0cd7d668aae807e5eb9c690efb3a3ed0117d1adc
argishh <abhangiargish@gmail.com>
2024-12-04 18:18:04 +0530

    update setup

--- setup.py
+++ setup.py
@@ -4,13 +4,14 @@ setup(
     name="genfunc",
     version="0.1.0",
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         "python-dotenv>=7.0.0",
     ],
     author="Argish",
     author_email="argish.official@gmail.com",
     description="A Python package for generating functions using LLMs",
-    long_description=open("docs/README.md").read(),
+    long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/argishh/genfunc",
     classifiers=[
